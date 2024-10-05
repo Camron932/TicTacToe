dda6f5b6e53328ca829fc1831ffa36a1c2d46e93
Camron M <Camron.Joseph.Mahler@gmail.com>
2024-10-05 19:16:55 -0400

    add click event to boxes

--- index.js
+++ index.js
@@ -5,15 +5,39 @@ const form = document.getElementById("myForm");
 
 newBtn.addEventListener("click", () => {
     formId.classList.remove("hidden");
-})
+});
 
 form.addEventListener('submit', (event) =>{
     event.preventDefault();
     const formData = new FormData(form);
     const data = Object.fromEntries(formData);
-    formId.classList.remove("hidden");
-    initializeGame(data)
-})
+    formId.classList.add("hidden");
+    initializeGame(data);
+});
+
+function initializeVariables(data) {
+    data.choice = +data.choice;
+    data.board = [1,2,3,4,5,6,7,8,9];
+    data.player1 = "X";
+    data.player2 = "O";
+    data.round = 0;
+    data.currentPlayer = "X";
+    data.gameOver = false;
+};
+
+function addEventListenersToGameBoard(data) {
+    document.querySelectorAll('.cubes').forEach((cubes) => {
+        cubes.addEventListener('click', (event) => {
+            playMove(event.target, data);
+        })
+    }) 
+}
 
 function initializeGame(data) {
+    initializeVariables(data);
+    addEventListenersToGameBoard(data);
+};
+
+function playMove(cubes, data) {
     
+};
